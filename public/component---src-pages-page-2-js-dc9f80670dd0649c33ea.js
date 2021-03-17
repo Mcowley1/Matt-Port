7f417c531413ad5c4ea07b4c198d885ab749341b
Cowleyfitcode <65091695+Mcowley1@users.noreply.github.com>
2021-03-17 09:30:51 -0400

    run clean

--- public/component---src-pages-page-2-js-dc9f80670dd0649c33ea.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkgatsby_starter_dimension_v2=self.webpackChunkgatsby_starter_dimension_v2||[]).push([[617],{9863:function(e,t,n){"use strict";n.r(t);var a=n(7294),l=n(5444),r=n(7198);t.default=function(){return a.createElement(r.Z,null,a.createElement("h1",null,"Hi from the second page"),a.createElement("p",null,"Welcome to page 2"),a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-dc9f80670dd0649c33ea.js.map
