8e7b3800ce674541a5a7d004f785ed0e33287ef4
Cowleyfitcode <65091695+Mcowley1@users.noreply.github.com>
2021-03-16 23:37:00 -0400

    added npm packages

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -1,13 +1,12 @@
-const { hot } = require("react-hot-loader/root")
 
 // prefer default export if available
 const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/matthewcowley/Desktop/Matt-Port/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/matthewcowley/Desktop/Matt-Port/src/pages/404.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/matthewcowley/Desktop/Matt-Port/src/pages/index.js"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/matthewcowley/Desktop/Matt-Port/src/pages/page-2.js")))
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/matthewcowley/Desktop/Matt-Port/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/matthewcowley/Desktop/Matt-Port/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/matthewcowley/Desktop/Matt-Port/src/pages/index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("/Users/matthewcowley/Desktop/Matt-Port/src/pages/page-2.js"))
 }
 
